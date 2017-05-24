af6bd7673888ef6986260bc4bdff60856a7644bf
yewenhe0904 <yewenhe0904@gmail.com>
2017-05-23 22:15:15 -0500

    init batch create pymongo

---
+++ py-mongo-batch-create.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import pprint, time
+
+host="192.168.1.188"
+port="27017"
+uri = "mongodb://yewenhe0904:k2cxtrb64vi@"+host+":"+port+"/test_db?authMechanism=SCRAM-SHA-1"
+client = MongoClient(uri)
+
+db=client.test_db
+collection=db.test
+
+# timer start
+start=time.time()
+for i in range(1000000):
+	collection.insert({"foo":"bar","baz":i,"z":10-i})
+# timer end
+total=time.time()-start
+print("%d seconds" % total)
+
+
--- py-mongo-connection.py
+++ py-mongo-connection.py
@@ -3,8 +3,10 @@ import pprint
 
 host="192.168.1.188"
 port="27017"
-uri = "mongodb://yewenhe0904:k2cxtrb64vi@192.168.1.188/test_db?authMechanism=SCRAM-SHA-1"
+uri = "mongodb://yewenhe0904:k2cxtrb64vi@"+host+":"+port+"/test_db?authMechanism=SCRAM-SHA-1"
 client = MongoClient(uri)
+
+
 db=client.test_db
 collection=db.test
 
